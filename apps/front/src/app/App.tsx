e76a0652b3b7119f29cbe224234c12e2e6de2640
Alexis Michiels <pro@alexismch.be>
2023-10-01 18:50:27 +0200

    feat(front): quote skeleton

--- apps/front/src/app/App.tsx
+++ apps/front/src/app/App.tsx
@@ -1,5 +1,42 @@
+/** @jsxImportSource @emotion/react */
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+   text-align: center;
+   min-height: 100vh;
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   flex-direction: column;
+   font-size: calc(10px + 2vmin);
+`;
+
+const Button = styled.button`
+   height: 50px;
+   width: 150px;
+   background: none;
+   border: 2px solid;
+   border-radius: 5px;
+   font-weight: bold;
+   font-size: 15px;
+   color: ${(props) => props.theme.colors.primary};
+   border-color: ${(props) => props.theme.colors.primary};
+   text-transform: uppercase;
+
+   :hover {
+      cursor: pointer;
+      color: ${(props) => props.theme.colors.background};
+      background: ${(props) => props.theme.colors.primary};
+   }
+`;
+
 export function App() {
-   return <div>Hello world!</div>;
+   return (
+      <Container>
+         <p>That's my quote</p>
+         <Button>Next</Button>
+      </Container>
+   );
 }
 
 export default App;
