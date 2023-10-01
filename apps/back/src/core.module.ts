e93f0473bdf2456a9b1d13ea8f90afb2cca17aa5
Alexis Michiels <pro@alexismch.be>
2023-10-01 22:19:59 +0200

    feat(back): enable CORS

--- apps/back/src/core.module.ts
+++ apps/back/src/core.module.ts
@@ -1,6 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { APIModule } from './API/API.module';
 import { DomainModule } from './Domain/Domain.module';
 
-@Module({ imports: [APIModule, DomainModule] })
+@Module({
+   imports: [
+      ConfigModule.forRoot({
+         isGlobal: true,
+         expandVariables: true,
+         cache: true,
+      }),
+      APIModule,
+      DomainModule,
+   ],
+})
 export class CoreModule {}
--- apps/back/src/main.ts
+++ apps/back/src/main.ts
@@ -1,14 +1,24 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 
 import { CoreModule } from './core.module';
 
 async function bootstrap() {
    const app = await NestFactory.create(CoreModule);
+   const configService = app.get(ConfigService);
+
+   // = CORS
+   app.enableCors({
+      origin: configService.get('SERVER_ORIGIN')
+         ? configService.get('SERVER_ORIGIN').split(',')
+         : '*',
+      credentials: true,
+   });
 
    app.enableShutdownHooks();
 
-   const port = process.env.PORT || 3000;
+   const port = configService.get('PORT') || 3000;
    await app.listen(port);
 
    Logger.log(`🚀 Application is running on: http://localhost:${port}`);
